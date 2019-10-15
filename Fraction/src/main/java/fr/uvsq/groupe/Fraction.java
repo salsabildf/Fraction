f17c231c26b57c8acdf8dd814ebb463dd60a2b86
Utilisateur <user@debian.arcahe.ovh>
2019-10-16 00:26:03 +0200

    ajout du constructeur par défaut

---
+++ Fraction/src/main/java/fr/uvsq/groupe/Fraction.java
@@ -0,0 +1,22 @@
+package fr.uvsq.groupe;
+
+public final class Fraction {
+	
+	private int denominateur;
+	private int numerateur;
+	
+	public Fraction()
+	{
+		denominateur = 0;
+	    numerateur = 1;
+	}
+		int getNum() {
+			return numerateur;
+		}
+		int getDen() {
+			return denominateur;
+		}
+		
+		
+		
+}
