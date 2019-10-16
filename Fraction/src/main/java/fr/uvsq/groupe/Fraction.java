9bac0432f5369fb857d4dfaad54242a327e9a404
Utilisateur <user@debian.arcahe.ovh>
2019-10-16 15:38:19 +0200

    modification .gitignore et class Fraction

--- Fraction/src/main/java/fr/uvsq/groupe/Fraction.java
+++ Fraction/src/main/java/fr/uvsq/groupe/Fraction.java
@@ -1,5 +1,22 @@
 package fr.uvsq.groupe;
 
-public class Fraction {
+public final class Fraction {
+	
+	private int numerateur;
+	private int denominateur;
+	
+	public Fraction(int num){
+		numerateur = num;
+		denominateur = 1;	
+	}
+
+	public int getNum() {
+		return numerateur;
+	}
+
+	public int getDen() {
+		return denominateur;
+	}
+
 
 }
--- Fraction/src/test/java/fr/uvsq/groupe/FractionTest.java
+++ Fraction/src/test/java/fr/uvsq/groupe/FractionTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 public class FractionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void constructeur_denominateur_par_defaut() {
+		Fraction f = new Fraction(5);
+		assertEquals (5, f.getNum());
+		assertEquals (1, f.getDen());
 	}
 
 }
